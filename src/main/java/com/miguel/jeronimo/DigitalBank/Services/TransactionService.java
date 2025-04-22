1118dd39727415f61219a3d7f25dedb64621645f
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-04-22 01:04:26 -0300

    Create Services

---
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Services/TransactionService.java
@@ -0,0 +1,89 @@
+package com.miguel.jeronimo.DigitalBank.Services;
+
+import com.miguel.jeronimo.DigitalBank.DTOs.TransactionDTO;
+import com.miguel.jeronimo.DigitalBank.Entities.Transaction;
+import com.miguel.jeronimo.DigitalBank.Entities.TransactionStatus;
+import com.miguel.jeronimo.DigitalBank.Repositories.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository repository;
+    private final TransactionProducer producer;
+
+    public TransactionService(TransactionRepository repository, TransactionProducer producer) {
+        this.repository = repository;
+        this.producer = producer;
+    }
+
+    public void createTransaction(TransactionDTO request) {
+        Transaction transaction = new Transaction();
+
+        try {
+            transaction.setSender(request.sender());
+            transaction.setReceiver(request.receiver());
+            transaction.setAmount(request.amount());
+
+            validateTransaction(request);
+            transaction.setStatus(TransactionStatus.PROCESSING);
+            repository.save(transaction);
+
+            processTransaction(transaction);
+
+
+        }catch (Exception e) {
+            transaction.setStatus(TransactionStatus.FAILED);
+            e.printStackTrace();
+        }finally {
+            repository.save(transaction);
+        }
+    }
+
+    public void validateTransaction(TransactionDTO request) throws Exception {
+
+        if(request.sender().getBalance().compareTo(request.amount()) < 0)
+            throw new Exception();
+
+        if(!request.receiver().isActive())
+            throw new Exception();
+    }
+
+
+    public void processTransaction(Transaction transaction) {
+        transaction.getSender().setBalance(transaction.getSender().getBalance().subtract(transaction.getAmount()));
+        transaction.getReceiver().setBalance(transaction.getReceiver().getBalance().subtract(transaction.getAmount()));
+        transaction.setStatus(TransactionStatus.COMPLETED);
+    }
+
+    public void refundTransaction(Long id) {
+        Optional<Transaction> transactionOptional = repository.findById(id);
+
+        LocalDateTime actualTime = LocalDateTime.now();
+
+        if(transactionOptional.isPresent() &&
+        transactionOptional.get().getDate().isBefore(actualTime.plusMinutes(10))) {
+            
+            Transaction transaction = transactionOptional.get();
+
+            transaction.getSender().setBalance(transaction.getSender().getBalance().add(transaction.getAmount()));
+            transaction.getReceiver().setBalance(transaction.getReceiver().getBalance().subtract(transaction.getAmount()));
+
+            transaction.setStatus(TransactionStatus.REFUNDED);
+            repository.save(transaction);
+        }
+    }
+    public void cancelTransaction(Long id) {
+        Optional<Transaction> transactionOptional = repository.findById(id);
+
+        if(transactionOptional.isPresent()) {
+            Transaction transaction = transactionOptional.get();
+            transaction.setStatus(TransactionStatus.CANCELLED);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Services/UserServices.java
@@ -0,0 +1,123 @@
+package com.miguel.jeronimo.DigitalBank.Services;
+
+import com.miguel.jeronimo.DigitalBank.Entities.PixKeyType;
+import com.miguel.jeronimo.DigitalBank.Entities.User;
+import com.miguel.jeronimo.DigitalBank.Repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServices {
+
+    private final UserRepository repository;
+
+    public UserServices(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createUser(User user) {
+
+        try{
+            validateUser(user);
+
+            setPixKey(user);
+            repository.save(user);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setPixKey(User user){
+        switch (user.getPixKeyType()){
+            case PixKeyType.CPF:
+                user.setPixKey(user.getCpf());
+                break;
+            case PixKeyType.EMAIL:
+                user.setPixKey(user.getEmail());
+                break;
+            case PixKeyType.RANDOM_KEY:
+                user.setPixKey(UUID.randomUUID().toString());
+                break;
+        }
+    }
+
+    public void validateUser(User user){
+
+        if(repository.findByCpf(user.getCpf()).isPresent())
+            throw new IllegalArgumentException("CPF already exists");
+
+        if(repository.findByCnpj(user.getCnpj()).isPresent())
+            throw new IllegalArgumentException("CNPJ already exists");
+
+        if(!user.isPerson()){
+            if(!validateCNPJ(user))
+                throw new IllegalArgumentException("CNPJ is invalid");
+        }else{
+            if(!validateCPF(user))
+                throw new IllegalArgumentException("CPF is invalid");
+        }
+
+    }
+
+    public void deleteUser(Long id, String password) throws Exception {
+        Optional<User> user = repository.findById(id);
+
+        if(user.isPresent()){
+            if(user.get().getPassword().equals(password)){
+                repository.deleteById(id);
+            }else {
+                throw new Exception("Wrong password");
+            }
+        }else{
+            throw new Exception("User not found");
+        }
+    }
+
+    public User findUser(String pixKey){
+        Optional<User> user = repository.findByPixKey(pixKey);
+        return user.orElse(null);
+    }
+
+    public boolean validateCPF(User user){
+        if (user.getCpf().length() != 11 || user.getCpf().matches("(\\d)\\1{10}")) return false;
+
+        int sum = 0, weight = 10;
+        for (int i = 0; i < 9; i++) sum += (user.getCpf().charAt(i) - '0') * weight--;
+        int digit1 = (sum * 10) % 11;
+        digit1 = (digit1 == 10) ? 0 : digit1;
+
+        sum = 0; weight = 11;
+        for (int i = 0; i < 10; i++) sum += (user.getCpf().charAt(i) - '0') * weight--;
+        int digit2 = (sum * 10) % 11;
+        digit2 = (digit2 == 10) ? 0 : digit2;
+
+        return digit1 == (user.getCpf().charAt(9) - '0') && digit2 == (user.getCpf().charAt(10) - '0');
+    }
+
+    public boolean validateCNPJ(User user){
+        if (user.getCnpj().length() != 14 || user.getCnpj().matches("(\\d)\\1{13}")) return false;
+
+        int[] pesos1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[] pesos2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+        int sum = 0;
+        for (int i = 0; i < 12; i++)
+            sum += (user.getCnpj().charAt(i) - '0') * pesos1[i];
+
+        int digit1 = sum % 11;
+        digit1 = (digit1 < 2) ? 0 : 11 - digit1;
+
+        sum = 0;
+        for (int i = 0; i < 13; i++)
+            sum += (user.getCnpj().charAt(i) - '0') * pesos2[i];
+
+        int digit2 = sum % 11;
+        digit2 = (digit2 < 2) ? 0 : 11 - digit2;
+
+        return user.getCnpj().charAt(12) - '0' == digit1 && user.getCnpj().charAt(13) - '0' == digit2;
+    }
+}
