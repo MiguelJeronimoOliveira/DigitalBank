44a1275712c65371d480339cf53adb213f570405
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-04-23 15:59:52 -0300

    Update UserEntity
    Update Table Name

--- src/main/java/com/miguel/jeronimo/DigitalBank/Entities/User.java
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Entities/User.java
@@ -1,14 +1,13 @@
 package com.miguel.jeronimo.DigitalBank.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.miguel.jeronimo.DigitalBank.Enums.PixKeyType;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
