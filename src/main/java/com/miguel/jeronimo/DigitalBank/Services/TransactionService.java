6f36e7445ca77bd961973b7034601c6deb173df5
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-04-24 22:22:59 -0300

    Update TransactionService
    Add: error logs and exceptions in try catch
    Create: updateBalances Method

--- src/main/java/com/miguel/jeronimo/DigitalBank/Services/TransactionService.java
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Services/TransactionService.java
@@ -2,8 +2,14 @@ package com.miguel.jeronimo.DigitalBank.Services;
 
 import com.miguel.jeronimo.DigitalBank.DTOs.TransactionDTO;
 import com.miguel.jeronimo.DigitalBank.Entities.Transaction;
-import com.miguel.jeronimo.DigitalBank.Entities.TransactionStatus;
+import com.miguel.jeronimo.DigitalBank.Entities.User;
+import com.miguel.jeronimo.DigitalBank.Enums.TransactionStatus;
+import com.miguel.jeronimo.DigitalBank.Exceptions.InsufficientBalanceException;
+import com.miguel.jeronimo.DigitalBank.Exceptions.UserNotFoundException;
 import com.miguel.jeronimo.DigitalBank.Repositories.TransactionRepository;
+import com.miguel.jeronimo.DigitalBank.Repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,51 +18,64 @@ import java.util.Optional;
 @Service
 public class TransactionService {
 
+    private static Logger log = LoggerFactory.getLogger(TransactionService.class);
+
     private final TransactionRepository repository;
     private final TransactionProducer producer;
+    private final UserRepository userRepository;
 
-    public TransactionService(TransactionRepository repository, TransactionProducer producer) {
+    public TransactionService(TransactionRepository repository, TransactionProducer producer, UserRepository userRepository) {
         this.repository = repository;
         this.producer = producer;
+        this.userRepository = userRepository;
     }
 
-    public void createTransaction(TransactionDTO request) {
+    public Transaction DTOToEntity(TransactionDTO request) {
         Transaction transaction = new Transaction();
 
+        User sender = userRepository.findById(request.sender().getId())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        User receiver = userRepository.findByPixKey(request.receiver().getPixKey())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        transaction.setSender(sender);
+        transaction.setReceiver(receiver);
+        transaction.setAmount(request.amount());
+
+        return transaction;
+    }
+
+    public void createTransaction(TransactionDTO request) throws InsufficientBalanceException {
+        Transaction transaction = DTOToEntity(request);
+
         try {
-            transaction.setSender(request.sender());
-            transaction.setReceiver(request.receiver());
-            transaction.setAmount(request.amount());
 
-            validateTransaction(request);
+            validateTransaction(transaction);
+
             transaction.setStatus(TransactionStatus.PROCESSING);
-            repository.save(transaction);
 
-            processTransaction(transaction);
+            producer.send("process-topic", transaction);
 
-
-        }catch (Exception e) {
+        }catch (InsufficientBalanceException e) {
             transaction.setStatus(TransactionStatus.FAILED);
-            e.printStackTrace();
+            log.error(e.getMessage());
+            throw e;
+        }catch (IllegalArgumentException e){
+            log.error(e.getMessage());
+            throw e;
         }finally {
             repository.save(transaction);
         }
     }
 
-    public void validateTransaction(TransactionDTO request) throws Exception {
+    public void validateTransaction(Transaction transaction){
 
-        if(request.sender().getBalance().compareTo(request.amount()) < 0)
-            throw new Exception();
+        if(transaction.getSender().getBalance().compareTo(transaction.getAmount()) < 0)
+            throw new InsufficientBalanceException("Insufficient balance");
 
-        if(!request.receiver().isActive())
-            throw new Exception();
-    }
-
-
-    public void processTransaction(Transaction transaction) {
-        transaction.getSender().setBalance(transaction.getSender().getBalance().subtract(transaction.getAmount()));
-        transaction.getReceiver().setBalance(transaction.getReceiver().getBalance().add(transaction.getAmount()));
-        transaction.setStatus(TransactionStatus.COMPLETED);
+        if(!transaction.getReceiver().isActive())
+            throw new IllegalArgumentException("User is not active");
     }
 
     public void refundTransaction(Long id) {
@@ -68,14 +87,12 @@ public class TransactionService {
         transactionOptional.get().getDate().isBefore(actualTime.plusMinutes(10))) {
             
             Transaction transaction = transactionOptional.get();
+            producer.send("refund-topic", transaction);
 
-            transaction.getSender().setBalance(transaction.getSender().getBalance().add(transaction.getAmount()));
-            transaction.getReceiver().setBalance(transaction.getReceiver().getBalance().subtract(transaction.getAmount()));
-
-            transaction.setStatus(TransactionStatus.REFUNDED);
             repository.save(transaction);
         }
     }
+
     public void cancelTransaction(Long id) {
         Optional<Transaction> transactionOptional = repository.findById(id);
 
@@ -85,5 +102,13 @@ public class TransactionService {
         }
     }
 
+    public void updateBalances(User sender, User receiver) {
+        userRepository.save(sender);
+        userRepository.save(receiver);
+    }
+
+    public void save(Transaction transaction) {
+        repository.save(transaction);
+    }
 
 }
