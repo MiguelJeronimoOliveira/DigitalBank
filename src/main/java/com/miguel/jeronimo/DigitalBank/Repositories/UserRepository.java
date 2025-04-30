50a81e82a47476ec0a04e47e9e9e0f144a5150a6
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-04-30 01:26:37 -0300

    Update Consumer
    Update Consumer to procress PIX transactions

--- src/main/java/com/miguel/jeronimo/DigitalBank/Repositories/UserRepository.java
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Repositories/UserRepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.pixKey = ?1 AND u.active = true")
+    @Query("SELECT u FROM User u WHERE u.cpf = ?1 AND u.active = true")
     Optional<User> findByCpf(String cpf);
 
     @Query("SELECT u FROM User u WHERE u.pixKey = ?1 AND u.active = true")
     Optional<User> findByPixKey(String pixKey);
 
-    @Query("SELECT u FROM User u WHERE u.pixKey = ?1 AND u.active = true")
+    @Query("SELECT u FROM User u WHERE u.cnpj = ?1 AND u.active = true")
     Optional<User> findByCnpj(String cnpj);
 }
--- src/main/java/com/miguel/jeronimo/DigitalBank/Services/TransactionConsumer.java
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Services/TransactionConsumer.java
@@ -1,30 +1,46 @@
 package com.miguel.jeronimo.DigitalBank.Services;
 
+import com.miguel.jeronimo.DigitalBank.Entities.Card;
+import com.miguel.jeronimo.DigitalBank.Entities.CardStatement;
 import com.miguel.jeronimo.DigitalBank.Entities.Transaction;
 import com.miguel.jeronimo.DigitalBank.Enums.TransactionStatus;
 import com.miguel.jeronimo.DigitalBank.Repositories.TransactionRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
 @Component
 public class TransactionConsumer {
 
-    private final TransactionService transactionService;
+    private final TransactionAuxService service;
 
-    public TransactionConsumer(TransactionService transactionService, TransactionRepository transactionRepository) {
-        this.transactionService = transactionService;
+    public TransactionConsumer(TransactionAuxService service) {
+        this.service = service;
     }
 
-    @KafkaListener(topics = "process-topic", concurrency = "2", groupId = "transaction-group")
+    @KafkaListener(topics = "processPix-topic", concurrency = "2", groupId = "transaction-group")
     public void processTransaction(Transaction transaction) {
 
         transaction.getSender().setBalance(transaction.getSender().getBalance().subtract(transaction.getAmount()));
         transaction.getReceiver().setBalance(transaction.getReceiver().getBalance().add(transaction.getAmount()));
 
-        transactionService.updateBalances(transaction.getSender(), transaction.getReceiver());
+        service.updateBalances(transaction.getSender(), transaction.getReceiver());
 
         transaction.setStatus(TransactionStatus.COMPLETED);
-        transactionService.save(transaction);
+        service.save(transaction);
+
+        System.out.println("transaction processed successfully");
+    }
+
+    @KafkaListener(topics = "processDebit-topic", groupId = "transaction-group")
+    public void processDebitTransaction(Transaction transaction) {
+        transaction.getSender().setBalance(transaction.getSender().getBalance().subtract(transaction.getAmount()));
+
+        transaction.setStatus(TransactionStatus.COMPLETED);
+        service.updateBalance(transaction.getSender());
 
         System.out.println("transaction processed successfully");
     }
@@ -35,10 +51,10 @@ public class TransactionConsumer {
         transaction.getSender().setBalance(transaction.getSender().getBalance().add(transaction.getAmount()));
         transaction.getReceiver().setBalance(transaction.getReceiver().getBalance().subtract(transaction.getAmount()));
 
-        transactionService.updateBalances(transaction.getSender(), transaction.getReceiver());
+        service.updateBalances(transaction.getSender(), transaction.getReceiver());
 
         transaction.setStatus(TransactionStatus.REFUNDED);
-        transactionService.save(transaction);
+        service.save(transaction);
         
         System.out.println("transaction refunded successfully");
     }
