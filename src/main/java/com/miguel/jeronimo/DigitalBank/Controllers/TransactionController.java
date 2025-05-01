8e06c2ba1974eae6a6e8cae19f1de0a2f392c9a0
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2025-05-01 20:36:22 -0300

    Update TransactionController
    Add: debit endPoint

--- src/main/java/com/miguel/jeronimo/DigitalBank/Controllers/TransactionController.java
+++ src/main/java/com/miguel/jeronimo/DigitalBank/Controllers/TransactionController.java
@@ -1,9 +1,8 @@
 package com.miguel.jeronimo.DigitalBank.Controllers;
 
 import com.miguel.jeronimo.DigitalBank.DTOs.CreditTransactionDTO;
+import com.miguel.jeronimo.DigitalBank.DTOs.DebitTransactionDTO;
 import com.miguel.jeronimo.DigitalBank.DTOs.TransactionDTO;
-import com.miguel.jeronimo.DigitalBank.Entities.Transaction;
-import com.miguel.jeronimo.DigitalBank.Exceptions.InsufficientBalanceException;
 import com.miguel.jeronimo.DigitalBank.Services.TransactionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +22,19 @@ public class TransactionController {
 
     @PostMapping
     public ResponseEntity postTransaction(@RequestBody TransactionDTO request) throws Exception {
-        service.createTransaction(request);
+        service.createPixTransaction(request);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/credit")
     public ResponseEntity postCreditTransaction(@RequestBody CreditTransactionDTO request) throws Exception {
         service.updateInstallment(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/debit")
+    public ResponseEntity ResponseEntitypostDebitTransaction(@RequestBody DebitTransactionDTO request){
+        service.createDebitTransaction(request);
         return ResponseEntity.ok().build();
     }
 
